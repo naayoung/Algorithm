096c07ab780addd8cfcb066de1b190ae239e7c31
Lee Na-young <ask5046a@gmail.com>
2023-09-11 15:44:07 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    even = 0
+    odd = 0
+    
+    for i in range(len(num_list)):
+        if(num_list[i]%2 == 0):
+            even += 1
+        else:
+            odd += 1
+    answer = [even, odd]
+    return answer
