ebd6bc95ef1ca2a843c3ec53974d24a6e40d6701
Lee Na-young <ask5046a@gmail.com>
2023-09-13 15:39:39 +0900

    [level 0] Title: k의 개수, Time: 9.67 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,9 +1,5 @@
 def solution(i, j, k):
     answer = 0
-    k = str(k)
-    
-    arr = list(map(str, range(i, j+1)))
-    arr_new = "".join(arr)
-    answer = arr_new.count(k)
-    
+    for n in range(i, j+1):
+        answer += str(n).count(str(k))
     return answer
