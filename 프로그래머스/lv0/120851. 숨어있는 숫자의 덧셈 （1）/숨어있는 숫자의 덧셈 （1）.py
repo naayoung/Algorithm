35c465611fc12ddda115b046b33fd8cb6f2d0316
Lee Na-young <ask5046a@gmail.com>
2023-09-18 22:36:01 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,8 @@
+import re
+
+def solution(my_string):
+    answer = 0
+    my_string = re.findall(r'\d', my_string)
+    my_string = list(map(int, my_string))
+    answer = sum(my_string)
+    return answer
