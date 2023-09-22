b8107d2c05b82e736cab584479b72aa2e06312d1
Lee Na-young <ask5046a@gmail.com>
2023-09-22 12:50:30 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.42 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -6,6 +6,8 @@ def solution(left, right):
         for i in range(1, j+1):
             if(j%i == 0):
                 count += 1
+                
+        #약수의 개수 판별        
         if(count%2 == 0):
             answer += j
         else:
