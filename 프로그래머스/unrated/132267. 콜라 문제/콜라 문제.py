46013ae1a3517914cb35d3acef06b1703f003af4
Lee Na-young <ask5046a@gmail.com>
2023-09-22 10:02:59 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -2,18 +2,13 @@ import sys
 sys.setrecursionlimit(10**7)
 
 answer = 0
-count = 0
 
 def solution(a, b, n):
     global answer
-    global count
     
     if(n < a):
-        """if(b > 1):
-            answer = answer*(count-1)*b"""
         return answer
     else:
         answer += (n//a)*b
         n = (n//a)*b+n%a
-        count += 1
         return solution(a, b, n)
