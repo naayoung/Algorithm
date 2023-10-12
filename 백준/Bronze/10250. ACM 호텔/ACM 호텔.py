5a6859e88d8fb090763f42ae4c4e8bb305c2b9c3
Lee Na-young <ask5046a@gmail.com>
2023-10-12 17:10:27 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -8,11 +8,10 @@ for i in range(t):
     h, w, n = list(map(int, input().split()))
     count = 0
     while True:
+        count += 1
         if n > h:
-            count += 1
             n = n - h
         elif n <= h:
-            count += 1
             answer = n*100+count
             print(answer)
             break
