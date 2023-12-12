4e40bf0a4c06e8d0ec91edc94afca9993bbd1038
nayoung <ask5046a@gmail.com>
2023-12-12 12:24:39 +0900

    [Silver III] Title: N과 M (1), Time: 116 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,9 @@
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+p = permutations(range(1, n+1), m)
+
+for i in p:
+    print(" ".join(map(str, i)))
