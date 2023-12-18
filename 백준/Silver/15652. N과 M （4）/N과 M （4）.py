377d3e7caf57eae27fe954ee42a375b07f144e91
nayoung <ask5046a@gmail.com>
2023-12-18 23:26:47 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def back(start):
+    if len(answer) == m:
+        print(" ".join(map(str, answer)))
+        return
+    for i in range(start, n+1):
+        answer.append(i)
+        back(i)
+        answer.pop()
+
+n, m = map(int, input().split())
+answer = []
+back(1)
