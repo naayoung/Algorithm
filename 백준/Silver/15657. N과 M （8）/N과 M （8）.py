ebfaeb24b80053b106dbb8befe2d82c45332c6bf
nayoung <ask5046a@gmail.com>
2024-01-01 02:04:24 +0900

    [Silver III] Title: N과 M (8), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+answer = []
+
+def back(start):
+    if len(answer) == m:
+        print(" ".join(map(str, answer)))
+        return
+    for i in range(start, n):
+        answer.append(num[i])
+        back(i)
+        answer.pop()
+
+back(0)
