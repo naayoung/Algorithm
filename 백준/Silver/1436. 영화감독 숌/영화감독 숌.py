965e40ec5bcb722c6ffabf059bfcd7fe47da5c18
nayoung <ask5046a@gmail.com>
2024-03-18 14:28:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 676 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+count = 0
+result = 666
+
+while True:
+    if '666' in str(result):
+        count += 1
+    if count == n:
+        break
+
+    result += 1
+print(result)
