da0df6dafcb2fd840fa189f48ade29c3104d7bca
nayoung <ask5046a@gmail.com>
2024-07-18 20:43:02 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 35672 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+max_distance = 100001
+visited = [-1] * max_distance
+q = deque()
+
+def in_range(x):
+    return 0 <= x < max_distance
+
+def can_go(x):
+    return in_range(x) and visited[x] == -1
+
+def bfs():
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(visited[x])
+            break
+        for nx in (x-1, x+1, x*2):
+            if can_go(nx):
+                if nx == x * 2:
+                    visited[nx] = visited[x]
+                    q.appendleft(nx)
+                else:
+                    visited[nx] = visited[x] + 1
+                    q.append(nx)
+q.append(n)
+visited[n] = 0
+bfs()
