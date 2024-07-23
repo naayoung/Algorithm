5cbeab9ce49256ad65b15543ff08b1f71e02f91c
nayoung <ask5046a@gmail.com>
2024-07-23 20:20:27 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 672 ms, Memory: 72852 KB -BaekjoonHub

--- 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -1,26 +1,34 @@
 import sys
-sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
+# 입력받기
 n, m, r = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
-depth = [-1] * (n+1)
 
-for i in range(n+1):
+# 깊이 배열 초기화
+depth = [-1] * (n + 1)
+
+# 각 노드의 인접 노드를 오름차순으로 정렬
+for i in range(1, n + 1):
     graph[i].sort()
 
-def dfs(x):
-    for nx in graph[x]:
-        if depth[nx] == -1:
-            depth[nx] = depth[x] + 1
-            dfs(nx)
+def dfs(start):
+    stack = [(start, 0)]  # 스택에 (노드, 깊이) 형태로 저장
+    while stack:
+        x, d = stack.pop()
+        if depth[x] == -1:  # 방문하지 않은 경우
+            depth[x] = d
+            for nx in reversed(graph[x]):  # 오름차순 방문을 위해 역순으로 넣기
+                if depth[nx] == -1:
+                    stack.append((nx, d + 1))
 
-depth[r] = 0
+# DFS 시작
 dfs(r)
 
-for i in range(1, n+1):
+# 결과 출력
+for i in range(1, n + 1):
     print(depth[i])
