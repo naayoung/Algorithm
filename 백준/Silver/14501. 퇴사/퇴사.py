d47bd441cc5b0c69549e75273c1d80a732f1f9d8
nayoung <ask5046a@gmail.com>
2024-07-25 18:59:48 +0900

    [Silver III] Title: 퇴사, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,18 +1,22 @@
 import sys
-
 input = sys.stdin.readline
 
-n = int(input())
-s = []
+def meet(idx, price):
+    global answer
 
-for i in range(n):
-    t, p = map(int, input().split())
-    s.append((t, p))
+    if idx == n:
+        answer = max(answer, price)
+        return
+    if idx > n-1:
+        return
 
-dp = [0 for i in range(n+1)]
+    meet(idx+plan[idx][0], price+plan[idx][1])
+    meet(idx+1, price)
 
-for i in range(n):
-    for j in range(i+s[i][0], n+1):
-        if dp[j] < dp[i]+s[i][1]:
-            dp[j] = dp[i]+s[i][1]
-print(dp[-1])
+n = int(input().strip())
+plan = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+meet(0, 0)
+
+print(answer)
