0f3b016e2149e76f3a7055d0def659a19214bd9a
nayoung <ask5046a@gmail.com>
2024-08-05 02:56:49 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,7 @@
 def solution(array, commands):
     answer = []
-    
-    for x,y,z in commands:
-        arr = array[x-1:y]
-        arr.sort()
-        answer.append(arr[z-1])   
+    for com in commands:
+        temp = array[com[0]-1:com[1]]
+        temp.sort()
+        answer.append(temp[com[2]-1])
     return answer
