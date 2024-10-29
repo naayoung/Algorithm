c2d27492037279d9f58a3105a916dd396cae06cf
naayoung <ask5046a@gmail.com>
2024-10-29 23:54:51 +0900

    [Bronze I] Title: 단어 공부, Time: 80 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -2,27 +2,18 @@ import sys
 input = sys.stdin.readline
 
 word = str(input()).strip().upper()
-ans = {}
-
-for i in word:
-    if i not in ans:
-        count = 1
-        ans[i] = count
-    else:
-        count = ans[i] + 1
-        ans[i] = count
-
-ans_values = list(ans.values())
-ans = sorted(ans.items(), key = lambda x : x[1], reverse = True)
+word_list = list(set(word))
+answer = []
 count = 0
 
-for i in ans:
-    if(max(ans_values) != i[1]):
-        continue
-    else:
+for i in word_list:
+    answer.append(word.count(i))
+
+for i in answer:
+    if max(answer) == i:
         count += 1
 
-if len(ans) == 1 or count == 1:
-    print(ans[0][0])
-else:
+if(count >= 2):
     print("?")
+else:
+    print(word_list[answer.index(max(answer))])
