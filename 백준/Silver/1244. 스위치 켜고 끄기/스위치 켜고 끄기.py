d1868b4084e38ac5b53ae52969225f06c26811b0
naayoung <ask5046a@gmail.com>
2024-10-30 22:33:00 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -4,18 +4,15 @@ input = sys.stdin.readline
 s = int(input().strip())
 switch = list(map(int, input().split()))
 stu = int(input().strip())
-
 for _ in range(stu):
     ss, num = map(int, input().split())
 
-    # 남학생
+    #남학생
     if ss == 1:
-        # 남학생은 num의 배수를 모두 변경
-        for i in range(num - 1, s, num):
+        for i in range(num-1, s, num):
             switch[i] = 1 - switch[i]
-
-    # 여학생
-    elif ss == 2:
+    #여학생
+    if ss == 2:
         # 여학생은 자신을 중심으로 대칭인 구간을 확인하며 변경
         num -= 1  # 인덱스를 0부터 시작하도록 조정
         left = right = num
@@ -26,6 +23,5 @@ for _ in range(stu):
             left -= 1
             right += 1
 
-# 출력
 for i in range(0, s, 20):
     print(*switch[i:i+20])
