e032febb14112d6ec0ae2821979328249d5279cc
naayoung <ask5046a@gmail.com>
2024-11-20 19:24:55 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -0,0 +1,40 @@
+def solution(video_len, pos, op_start, op_end, commands):
+    def str_to_int(time):
+        mm, ss = time.split(':')
+        mm, ss = int(mm), int(ss)
+        time = mm*60+ss
+        return time
+    
+    answer = ''
+    
+    video_len_time = str_to_int(video_len)
+    pos_time = str_to_int(pos)
+    op_start_time = str_to_int(op_start)
+    op_end_time = str_to_int(op_end)
+    
+    for command in commands:
+        #오프닝 구간일 경우, 오프닝이 끝나는 위치로
+        if op_start_time <= pos_time <= op_end_time:
+            pos_time = op_end_time
+        if command == 'prev':
+            pos_time -= 10
+            #10초 미만인 경우, 영상의 처음 위치로
+            if pos_time < 0:
+                pos_time = 0
+        if command == 'next':
+            pos_time += 10
+            #남은 시간이 10초 미만인 경우, 영상의 마지막 위치로
+            if pos_time > video_len_time:
+                pos_time = video_len_time
+                
+        if op_start_time <= pos_time <= op_end_time:
+            pos_time = op_end_time
+            
+    mm, ss = str(pos_time//60), str(pos_time%60)
+    if len(mm) == 1:
+        mm = '0'+str(int(mm))
+    if len(ss) == 1:
+        ss = '0'+str(int(ss))
+    answer = mm+':'+ss
+    
+    return answer
