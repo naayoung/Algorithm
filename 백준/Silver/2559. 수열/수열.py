883a1f874e449e52b3cac13c0ea05e8e3682fb7a
naayoung <ask5046a@gmail.com>
2024-11-28 01:02:54 +0900

    [Silver III] Title: 수열, Time: 88 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -8,7 +8,7 @@ prefix = [0 for _ in range(n+1)]
 for i in range(n):
     prefix[i+1] = prefix[i] + num[i]
 
-answer = -float('inf')
+answer = []
 for i in range(k, n+1):
-    answer = max(answer, prefix[i]-prefix[i-k])
-print(answer)
+    answer.append(prefix[i]-prefix[i-k])
+print(max(answer))
