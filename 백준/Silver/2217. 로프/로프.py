72868021e7c34fb27a8e15bd35eee7006d7cad22
naayoung <ask5046a@gmail.com>
2025-03-31 23:26:47 +0900

    [Silver IV] Title: 로프, Time: 104 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+rope = []
+
+for i in range(n):
+    rope.append(int(input()))
+rope.sort(reverse=True)
+
+for i in range(n):
+    rope[i] = rope[i] * (i+1)
+
+print(max(rope))
