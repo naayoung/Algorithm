f2c1a02067e7a499b7d37a631be55b232111abb7
naayoung <ask5046a@gmail.com>
2025-04-03 23:33:25 +0900

    [Silver I] Title: 안전 영역, Time: 1076 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,45 +1,50 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 n = int(input().strip())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[0] * n for _ in range(n)]
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    return 0 <= x and x < n and 0 <= y and y < n
 
-def can_go(x, y, k, visited):
-    return in_range(x, y) and visited[x][y] == 0 and graph[x][y] > k
+def can_go(x, y, k):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] == 1 or graph[x][y] <= k:
+        return False
+    return True
 
-def dfs(start_x, start_y, k, visited):
-    stack = [(start_x, start_y)]
-    visited[start_x][start_y] = 1
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    
+def dfs(x, y, k):
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+    stack = [(x, y)]
+    visited[x][y] = 1
+
     while stack:
         x, y = stack.pop()
-        for dx, dy in directions:
+        for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
-            if can_go(nx, ny, k, visited):
+            if can_go(nx, ny, k):
                 visited[nx][ny] = 1
                 stack.append((nx, ny))
 
-def count_safe_areas(k):
-    visited = [[0] * n for _ in range(n)]
+def safe_area(k):
+    global visited
     count = 0
-    
+    visited = [[0] * n for _ in range(n)]
+
     for i in range(n):
         for j in range(n):
-            if can_go(i, j, k, visited):
-                dfs(i, j, k, visited)
+            if can_go(i, j, k):
+                dfs(i, j, k)
                 count += 1
-    
     return count
 
 max_count = 0
-for k in range(101):
-    max_count = max(max_count, count_safe_areas(k))
+max_height = max(map(max, graph))
+for k in range(max_height):
+    max_count = max(max_count, safe_area(k))
 
 print(max_count)
