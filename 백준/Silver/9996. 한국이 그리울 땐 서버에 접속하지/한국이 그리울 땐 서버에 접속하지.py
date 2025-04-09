6b1403db76bcb1cc8acbd52b49dbcf106505a780
naayoung <ask5046a@gmail.com>
2025-04-09 23:53:26 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
@@ -2,21 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input().strip())
-pre, pos = input().strip().split("*")
+first, last = input().strip().split("*")
 
-ans = []
 for _ in range(N):
     temp = input().strip()
 
-    # 길이 자체가 짧으면 무조건 실패
-    if len(temp) < len(pre) + len(pos):
-        ans.append("NE")
-        continue
-
-    if temp.startswith(pre) and temp.endswith(pos):
-        ans.append("DA")
+    if len(temp) < len(first) + len(last):
+        print("NE")
+    elif temp[:len(first)] == first and temp[-len(last):] == last:
+        print("DA")
     else:
-        ans.append("NE")
-
-for a in ans:
-    print(a)
+        print("NE")
