cd495f831a620a8db18b2b86f380e3e8efbcdf7f
naayoung <ask5046a@gmail.com>
2025-04-24 23:55:07 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 940 ms, Memory: 44128 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.py
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.py
@@ -1,26 +1,20 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
+INF = int(1e9)
+MAX = N+N//2+1
+dp = [INF]*MAX
+dp[0] = 0
 
-# 도달할 수 있는 최대 값은 N + N//2 (이를 넘지 않도록 설정)
-MAX = N + N // 2 + 1
+for i in range(0, N+1):
+    if dp[i] == INF:
+        continue
+    if i+1 < MAX:
+        dp[i+1] = min(dp[i+1], dp[i]+1)
+    if i+i//2 < MAX:
+        dp[i+i//2] = min(dp[i+i//2], dp[i]+1)
 
-# dp 배열: dp[i]는 i번째 계단에 도달할 때 최소 K번 이하로 도달할 수 있는 횟수
-dp = [float('inf')] * MAX
-dp[0] = 0  # 처음에는 0번 계단에 0번으로 도달
-
-# BFS
-queue = deque([0])  # 시작점은 0
-while queue:
-    cur = queue.popleft()
-    for next_step in [cur + 1, cur + cur // 2]:  # +1, +현재//2
-        if next_step < MAX and dp[next_step] > dp[cur] + 1:
-            dp[next_step] = dp[cur] + 1
-            queue.append(next_step)
-
-# dp[N]이 K 이하이면 minigimbob, 아니면 water
 if dp[N] <= K:
     print("minigimbob")
 else:
