003d389efe0f5a97261b72ca2c8191a804fb6783
naayoung <ask5046a@gmail.com>
2025-06-12 00:54:26 +0900

    [Silver I] Title: 그림, Time: 424 ms, Memory: 37672 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -3,29 +3,28 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-picture = []
+graph = []
 for _ in range(n):
-    picture.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
 visited = [[0]*m for _ in range(n)]
 
-def in_range(row, col):
-    return 0 <= row < n and 0 <= col < m
+def in_range(r, c):
+    return 0 <= r < n and 0 <= c < m
 
-def can_go(row, col):
-    if not in_range(row, col):
+def can_go(r, c):
+    if not in_range(r, c):
         return False
-    if visited[row][col] == 1 or picture[row][col] == 0:
+    if visited[r][c] == 1 or graph[r][c] == 0:
         return False
     return True
 
-def bfs(row, col):
+def bfs(r, c):
     drs, dcs = [0, 1, 0, -1], [1, 0, -1, 0]
     q = deque()
-    q.append((row, col))
-    visited[row][col] = 1
+    q.append((r, c))
+    visited[r][c] = 1
 
     size = 1
-
     while q:
         r, c = q.popleft()
         for dr, dc in zip(drs, dcs):
@@ -34,20 +33,20 @@ def bfs(row, col):
             if can_go(nr, nc):
                 visited[nr][nc] = 1
                 q.append((nr, nc))
-                size += 1
+                size += 1 
     return size
+ 
 
 
-answer = []
+ans = []
 for r in range(n):
     for c in range(m):
         if can_go(r, c):
-            answer.append(bfs(r, c))
+            ans.append(bfs(r, c))
 
-
-if answer:
-    print(len(answer))
-    print(max(answer))
+if len(ans) != 0:
+    print(len(ans))
+    print(max(ans))
 else:
     print(0)
     print(0)
