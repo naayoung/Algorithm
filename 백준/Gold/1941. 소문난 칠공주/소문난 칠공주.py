f781fd6cfa3a363715fcd7f026e5fbcd27eea012
naayoung <ask5046a@gmail.com>
2025-07-07 22:21:58 +0900

    [Gold III] Title: 소문난 칠공주, Time: 1696 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,47 +1,40 @@
 import sys
 input = sys.stdin.readline
 
-def possible_next(selected):
-    moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    possible = set()
+# 상하좌우 이동
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
-    for x, y in selected:
-        for dx, dy in moves:
-            nx, ny = x+dx, y+dy
-            if 0 <= nx < 5 and 0 <= ny < 5:
-                if (nx, ny) not in selected:
-                    possible.add((nx, ny))
-    return list(possible)
+# 입력
+girls = [list(input().strip()) for _ in range(5)]
 
-def backtrack(selected, count_y):
+ans = set()
+
+def dfs(selected, count_y):
     if count_y > 3:
         return
     if len(selected) == 7:
-        key = tuple(sorted(selected))
-        if key not in answers:
-            answers.add(key)
+        ans.add(tuple(sorted(selected)))
         return
 
-    for nx, ny in possible_next(selected):
-        if (nx, ny) not in selected:
-            selected.append((nx, ny))
-            if girls[nx][ny] == 'Y':
-                backtrack(selected, count_y+1)
-            else:
-                backtrack(selected, count_y)
-            selected.remove((nx, ny))
+    # 현재 선택한 좌표들의 인접 칸 탐색
+    next_positions = set()
+    for x, y in selected:
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+            if 0 <= nx < 5 and 0 <= ny < 5:
+                if (nx, ny) not in selected:
+                    next_positions.add((nx, ny))
 
-girls = []
-for _ in range(5):
-    girls.append(list(input().strip()))
+    for nx, ny in next_positions:
+        selected.add((nx, ny))
+        dfs(selected, count_y + 1 if girls[nx][ny] == 'Y' else count_y)
+        selected.remove((nx, ny))
 
-answers = set()
+# 모든 좌표에서 시작
 for i in range(5):
     for j in range(5):
-        selected = set()
-        selected = [(i, j)]
-        if girls[i][j] == 'Y':
-            backtrack(selected, 1)
-        else:
-            backtrack(selected, 0)
-print(len(answers))
+        start = {(i, j)}
+        dfs(start, 1 if girls[i][j] == 'Y' else 0)
+
+print(len(ans))
